e3c8307423819968f01e1ab07ade5f74f5f9929d
Andre <andreraizdetudo@gmail.com>
2022-07-26 15:39:55 -0300

    criando nossa camada de view (template)  com javaScript

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,16 +4,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update();
     }
     adiciona(event) {
         event.preventDefault();
         
-        const negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade,
-            this._inputValor
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+               DateHelper.textoParaData(this._inputData.value),
+               this._inputQuantidade,
+               this._inputValor
         );
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    }
+    _limpaFormulario(){
+        this._inputData.value = "" ;
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
