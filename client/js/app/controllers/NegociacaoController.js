bbe9a01e2c190df18e206512b58fe0617870c4c3
Andre <andreraizdetudo@gmail.com>
2022-07-28 23:35:42 -0300

    Criando o nosso data bind - utilizando padrão Factory para encapsular a criação de um objeto complexo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,58 +5,44 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        const self = this;
-        /*
-        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
-        */
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
-            get(target,prop,receiver){
-                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-                    return function(){
-                        console.log(`intenceptando ${prop}`);
-                        Reflect.apply(target[prop],target,arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);
-            }
-        });
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
+        );
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(), 
+            new MensagemView($('#mensagemView')),
+            'texto'
+        );
     }
     adiciona(event) {
 
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-        
         this._limpaFormulario();
     }
 
-    apaga(){
+    apaga() {
 
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
-   
-    _criaNegociacao(){
+
+    _criaNegociacao() {
 
         return new Negociacao(
-               DateHelper.textoParaData(this._inputData.value),
-               this._inputQuantidade.value,
-               this._inputValor.value
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
         );
     }
-    _limpaFormulario(){
-        
-        this._inputData.value = "" ;
+    _limpaFormulario() {
+
+        this._inputData.value = "";
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
